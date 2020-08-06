f1563f4cd260f7ad5b42820d5cb3286490dc62ea
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-08-06 12:25:59 +0100

    more on perf tests

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject com.brunobonacci/reservoir "0.1.0-SNAPSHOT"
+(defn ver [] "0.1.0-SNAPSHOT")
+(defn ts  [] (System/currentTimeMillis))
+(defn jdk [] (clojure.string/replace (str (System/getProperty "java.vm.vendor") "-" (System/getProperty "java.vm.version")) #" " "_"))
+
+(defproject com.brunobonacci/reservoir #=(ver)
   :description "A fast implementation of Reservoir Sampling with Immutable Persistent data structures."
 
   :url "https://github.com/BrunoBonacci/reservoir"
@@ -19,13 +23,26 @@
                                   [org.clojure/test.check "1.0.0"]
                                   [criterium "0.4.5"]
                                   [org.slf4j/slf4j-log4j12 "1.7.30"]
-                                  [jmh-clojure "0.2.1"]
+                                  [jmh-clojure "0.3.1"]
                                   ;; for comparison
                                   [bigml/sampling "3.2"]
                                   ]
                    :resource-paths ["dev-resources"]
-                   :plugins      [[lein-midje "3.2.2"]]}}
+                   :plugins      [[lein-midje "3.2.2"]
+                                  [lein-jmh "0.2.8"]]}}
 
   :aliases
-  {"perf" ["with-profile" "dev" "jmh" #=(pr-str {:file "./dev/perf/benchmarks.edn" :status true :pprint true})]}
+  {"perf-quick"
+   ["with-profile" "dev" "jmh"
+    #=(pr-str {:file "./dev/perf/benchmarks.edn"
+               :status true :pprint true :format :table
+               :fork 1 :measurement 5
+               :output #=(clojure.string/join "-" ["./reservoir" #=(ver) #=(jdk) #=(ts) "results.edn"])})]
+
+   "perf"
+   ["with-profile" "dev" "jmh"
+    #=(pr-str {:file "./dev/perf/benchmarks.edn"
+               :status true :pprint true :format :table
+               :output #=(clojure.string/join "-" ["./reservoir" #=(ver) #=(jdk) #=(ts) "results.edn"])})]
+   }
   )
